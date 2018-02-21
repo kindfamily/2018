7210122e93f939c1150ee493fd720524c390efa4
bill <bill@billui-MacBook-Pro.local>
2018-02-22 01:36:33 +0900

    풀사이즈 제작중 - 네비 사이즈 완성 hr에 그림자 주는 단계, 전체 사이즈에 꽉 맞게 하는 과정

--- project1-dongnacom/js/app.js
+++ project1-dongnacom/js/app.js
@@ -34,7 +34,7 @@
     var user = {
         "name": "william",
         "subName": "bill",
-        "logPic": "image/Logo.jpg",
+        "logPic": "image/dongnecomlogo.png",
         "headerImg": "http://via.placeholder.com/1820x400",
         "contact": {
             "mobile": "01049055202",
@@ -49,8 +49,8 @@
     
 var projects = {};
 projects.headerfooter = function(){
-        var HTMLheaderName = '<h1 id="name" class="supertitle text-uppercase"><strong>%data%</strong></h1>';
-        var HTMLheaderSubName = '<h2 id="subname" class="supertitle text-uppercase">%data%</h2>';
+        // var HTMLheaderName = '<h2 id="name" class="supertitle text-uppercase"><strong>%data%</strong></h2>';
+        // var HTMLheaderSubName = '<h3 id="subname" class="supertitle text-uppercase">%data%</h3>';
         var HTMLheaderLogo = '<img src="%data%" class="img-responsive logo" alt="logo image">';
         var HTMLheaderImg = '<img src="%data%" class="img-responsive placeholdimg" alt="header image">';
         
@@ -58,15 +58,15 @@ projects.headerfooter = function(){
     
     
         // var forMattedName = HTMLheaderName.replace("%data%", edu.school[0].name);
-        var forMattedName = HTMLheaderName.replace("%data%", user.name);
-        var forMattedSubName = HTMLheaderSubName.replace("%data%", user.subName);
+        // var forMattedName = HTMLheaderName.replace("%data%", user.name);
+        // var forMattedSubName = HTMLheaderSubName.replace("%data%", user.subName);
         var forMattedLogo = HTMLheaderLogo.replace("%data%", user.logPic);
         var forMattedHeaderImg = HTMLheaderImg.replace("%data%", user.headerImg);
 
         var forMattedGithub = HTMLgithub.replace("%data%", user.contact.github);
     
-        $('#header').append(forMattedName);
-        $('#header').append(forMattedSubName);
+        // $('#header').append(forMattedName);
+        // $('#header').append(forMattedSubName);
         $('#logo').append(forMattedLogo);
         $('#headerimg').append(forMattedHeaderImg);
         
